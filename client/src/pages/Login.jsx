8affb2b546536f6b838331f8d948be529e04004b
Iulian Florin Bunau <iulianf30@gmail.com>
2024-07-03 15:53:24 +0300

    Primul commit

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import React from 'react'
+
+export default function login() {
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+
+const loginUser = (e) => {
+  e.preventDefault()
+}
+
+  return (
+    <div>
+      <form onSubmit={loginUser}>
+       <label>Email</label>
+        <input type='email' placeholder='enter email...'value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
+        <label> Password</label>
+        <input type='password' placeholder='enter password...'value={data.password} onChange={(e) => setData({...data, password: e.target.value})}/>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  )
+}
