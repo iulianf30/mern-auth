795bdced7bb98c44adc2fbc405d0ebe763a5bb39
Iulian Florin Bunau <iulianf30@gmail.com>
2024-07-08 14:23:00 +0300

    Criptare Parola

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const { hashPassword, comparePassword } = require ('../helpers/auth')
 
 const test = (req, res) => {
     res.json('test is working')
@@ -27,9 +28,13 @@ const registerUser = async (req, res) => {
             })
         }
 
+        const hashedPassword = await hashPassword(password)
+        //create user in database
         const user = await User.create({
-            name, email, password
-        })
+            name, 
+            email, 
+            password: hashedPassword,
+        });
 
         return res.json(user)
     } catch (error) {
